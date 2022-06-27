efd5cb3a073d201ba63929df6e1f1272494950d1
sadique-cws <sadique.hussain96@gmail.com>
2022-06-27 17:00:20 +0530

    new post dialog work done

--- src/components/PostPageHeader.jsx
+++ src/components/PostPageHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppBar, Box, Container, Grid, IconButton, InputAdornment, TextField, Input, Toolbar, Typography } from '@mui/material'
 
-const PostPageHeader = () => {
+const PostPageHeader = ({handleOpen}) => {
 
     const styles = {
         header: {
@@ -33,7 +33,7 @@ const PostPageHeader = () => {
                         <IconButton>
                         <svg aria-label="Messenger" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M12.003 2.001a9.705 9.705 0 110 19.4 10.876 10.876 0 01-2.895-.384.798.798 0 00-.533.04l-1.984.876a.801.801 0 01-1.123-.708l-.054-1.78a.806.806 0 00-.27-.569 9.49 9.49 0 01-3.14-7.175 9.65 9.65 0 0110-9.7z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1.739"></path><path d="M17.79 10.132a.659.659 0 00-.962-.873l-2.556 2.05a.63.63 0 01-.758.002L11.06 9.47a1.576 1.576 0 00-2.277.42l-2.567 3.98a.659.659 0 00.961.875l2.556-2.049a.63.63 0 01.759-.002l2.452 1.84a1.576 1.576 0 002.278-.42z" fill-rule="evenodd"></path></svg>
                         </IconButton>
-                        <IconButton>
+                        <IconButton onClick={handleOpen}>
                         <svg aria-label="New post" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line></svg>
                         </IconButton>
                         <IconButton>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid, Input, TextField } from '@mui/material'
+import React, { useState } from 'react'
 import PostPageBody from '../components/PostPageBody'
 import PostPageHeader from '../components/PostPageHeader'
 
+
+const InputButton = () => {
+  const ref = React.useRef();
+
+  return ( 
+   <>
+    <Button variant='contained' fullWidth onClick={() => ref.current.click()}>Upload</Button>
+    <input type='file' ref={ref} style={{display:"none"}}/> 
+    </>
+  )
+
+}
+
+const PostInsert = ({isOpen,handleClose}) => {
+
+  return (
+    <Dialog open={isOpen} maxWidth="xs" onClose={handleClose}>
+      <DialogTitle sx={{textAlign:"center"}}>
+          Insert New Post 
+      </DialogTitle>
+      <Divider/>
+        <DialogContent fullWidth>
+            <Grid container spacing={1}>
+                <Grid lg={4} item>
+                    <img src='https://via.placeholder.com/100x100.png' style={{width:"100%"}}/>
+                 
+                    <InputButton/>
+                </Grid>
+                <Grid item lg={8}>
+                    <TextField fullWidth multiline rows={6} placeholder="What`s in your mind?" variant='filled'  InputProps={{sx:{border:0},disableUnderline:true}}/>
+                </Grid>
+            </Grid>
+        </DialogContent>
+        <DialogActions>
+            <Button color="primary" variant='contained' fullWidth>Create new Post</Button>
+        </DialogActions>
+    </Dialog>
+  )
+}
+
+
 const PostPage = () => {
+  const [isOpen, setisOpen] = useState(false)
+
+  const handleOpen = () => {
+    setisOpen(true)
+  }
+
+  const handleClose = () => {
+    setisOpen(false)
+  }
   return (
     <div>
-      <PostPageHeader/>
+      <PostPageHeader handleOpen={handleOpen}/>
       <PostPageBody/>
+      <PostInsert isOpen={isOpen} handleClose={handleClose}/>
     </div>
   )
 }
